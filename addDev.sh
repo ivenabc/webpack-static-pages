0d6723a1b848b328b6dd044387db1c0891443a8e
ivenabc <hopeicc@163.com>
2018-06-19 15:44:57 +0800

    add dev.sh

---
+++ addDev.sh
@@ -0,0 +1,3 @@
+yarn add webpack webpack-cli webpack-dev-server --dev
+yarn add html-webpack-plugin clean-webpack-plugin extract-text-webpack-plugin --dev 
+yarn add file-loader style-loader css-loader --dev
